1c3cce6da9671d5c995da9d1cdb4fc72e08d9c60
mdmccoy <mdmccoy@gmail.com>
2018-03-21 09:09:15 -0400

    Rails new + start of tutorial.

---
+++ spec/features/visiting_root_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "Visiting root" do
+  it 'when not logged in redirects to a registration page' do
+    visit root_path
+    exepct(current_path).to eq('/register')
+  end
+
+  it 'when logged in shows a welcome message'
+end
